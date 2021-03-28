712abdd17228c95c414674935397826981441979
monaim5 <thexblackcat@gmail.com>
2021-03-28 03:20:14 +0200

    re architect

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
 $(document).ready(function() {
     var dataset = [];
+    console.log('hello')
     $.getJSON("scrape.json", function( data ) {
-              
-        $.each( data, function( key, val ) {
-            var row =[];
-            row.push(val.Ticker);
-            row.push(val.Company);
-            row.push(val.Exchange);
-            row.push(val.Float);
-            row.push(val.Outstd);
-            row.push(val.ShortInt);
-            row.push(val.Industry);
-
-            dataset.push(row);
-        });
-
+//        $.each( data, function( key, val ) {
+//            var row =[];
+//            row.push(val.Ticker);
+//            row.push(val.Company);
+//            row.push(val.Exchange);
+//            row.push(val.Float);
+//            row.push(val.Outstd);
+//            row.push(val.ShortInt);
+//            row.push(val.Industry);
+//
+//            dataset.push(row);
+//        });
+        console.log(data);
             $('#example').DataTable({
-data: dataset,
-columns: [
-    { title:  "Ticker"},
-    { title:   "Company"},
-    { title:   "Exchange"},
-    { title:  "Float"},
-    { title:  "Outstd"},
-    { title:  "ShortInt"},
-    { title:  "Industry"}
-    
-    ]
+data: data.data,
+columns: data.columns
 
 });
          
